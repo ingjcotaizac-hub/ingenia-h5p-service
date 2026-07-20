339a99a829e91d3aa3cec61a23a16cf1785949b3
Juan Carlos Otaiza Chirinos <ing.jcotaizac@gmail.com>
2026-07-20 10:35:17 +0200

    chore: clean up temp files and submodule to unblock railway deployment

--- test-editor.js
+++
@@ -1,3 +0,0 @@
-const H5P = require('@lumieducation/h5p-server');
-const h5pEditor = new H5P.H5PEditor({}, {}, {}, {}, {});
-console.log(h5pEditor.packageImporter);
--- test-importer.js
+++
@@ -1,2 +0,0 @@
-const H5PPackageImporter = require('@lumieducation/h5p-server/build/src/PackageImporter').default;
-console.log(typeof H5PPackageImporter);
